37e6a01190934cb9ad5853d1682741ca23e3fc41
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2022-06-30 17:39:17 -0300

    Update AbastecimentoForm.swift
    Incluido form

--- projetoCarro/Features/View/AbastecimentoForm.swift
+++ projetoCarro/Features/View/AbastecimentoForm.swift
@@ -11,7 +11,52 @@ struct AbastecimentoForm: View {
     let calendar = Calendar.current
 let date = Date()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView
+        {
+            Form
+            {
+                // TODO: arrumar formatacao
+                Section(header: Text("Display"),
+                        footer: Text("System settings will override Dark Mode and use the current device theme")) {
+                    Toggle(isOn: $darkModeEnabled,
+                           label: {
+                            Text("Dark mode")
+                           })
+                        .onChange(of: darkModeEnabled,
+                                  perform: { _ in
+                                    themeManager.handleTheme(darkMode: darkModeEnabled,
+                                                                  system: systemThemeEnabled)
+
+                                  })
+                    Toggle(isOn: $systemThemeEnabled,
+                           label: {
+                            Text("Use system settings")
+                           })
+                        .onChange(of: systemThemeEnabled,
+                                  perform: { _ in
+                                    themeManager.handleTheme(darkMode: darkModeEnabled,
+                                                                  system: systemThemeEnabled)
+
+                                  })
+                }
+                
+                Section {
+                    Link(destination: URL(string: Constants.twitter)!,
+                         label: {
+                            Label("Follow me on Twitter @tundsdev", systemImage: "link")
+                    })
+                    
+                    Link("Contact me via email",
+                         destination: URL(string: Constants.email)!)
+                    
+                    Link("Call me",
+                         destination: URL(string: Constants.phone)!)
+                }
+                .foregroundColor(Theme.textColor)
+                .font(.system(size: 16, weight: .semibold))
+            }
+            .navigationTitle("Settings")
+        }
     }
 }
 
