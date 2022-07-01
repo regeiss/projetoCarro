ad81f7859b35b01dcb5d9eca0f6142ab38e510b1
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2022-07-01 11:24:20 -0300

    Update MenuScreen.swift

--- projetoCarro/Features/Screen/MenuScreen.swift
+++ projetoCarro/Features/Screen/MenuScreen.swift
@@ -7,14 +7,41 @@
 
 import SwiftUI
 
+struct Photo: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
+let samplePhotos = (1...20).map { Photo(name: "coffee-\($0)") }
+
 struct MenuScreen: View
 {
-    private var gridItemLayout = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    
+    @State var gridLayout: [GridItem] = [ GridItem() ]
     
     var body: some View
     {
-        LazyVGrid(columns: gridItemLayout)
-        {}
+        NavigationView {
+    ScrollView {
+        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+
+            ForEach(samplePhotos.indices) { index in
+
+                Image(samplePhotos[index].name)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .frame(height: 200)
+                    .cornerRadius(10)
+                    .shadow(color: Color.primary.opacity(0.3), radius: 1)
+
+            }
+        }
+        .padding(.all, 10)
+    }
+
+    .navigationTitle("Coffee Feed")
+}
     }
 }
 
