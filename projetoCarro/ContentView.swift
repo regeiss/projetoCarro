b92223d1fcd90ffc5e28e823844af0ebebc222d3
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2022-07-12 17:57:22 -0300

    Update ContentView.swift
    incluido stackview

--- projetoCarro/ContentView.swift
+++ projetoCarro/ContentView.swift
@@ -6,15 +6,23 @@
 //
 
 import SwiftUI
+import NavigationStack
 
-struct ContentView: View {
-    var body: some View {
-        MenuScreen()
+struct ContentView: View 
+{
+    var body: some View 
+    {
+        NavigationStackView (transitionType: .custom(.scale), easing: .spring(response: 0.5, dampingFraction: 0.25, blendDuration: 0.5))
+        {
+            MenuScreen()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
+struct ContentView_Previews: PreviewProvider 
+{
+    static var previews: some View 
+    {
         ContentView()
     }
 }
