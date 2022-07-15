892388c669161df6a6622dd179d1a02fb7bf5da4
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2022-07-15 15:02:59 -0300

    Update Alertas.swift
    Incluido conteudo

--- projetoCarro/Features/View/Alertas.swift
+++ projetoCarro/Features/View/Alertas.swift
@@ -6,15 +6,29 @@
 //
 
 import SwiftUI
+import NavigationStack
 
-struct Alertas: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct Alertas: View 
+{
+    var body: some View 
+    {
+        PopView 
+        {
+            Text("<")
+        }
+
+        ForEach(0 ..< 6) { item in
+        RoundedRectangle(cornerRadius: 10)
+	        .fill(Color.blue)
+	        .frame(height: 100)
+        }
     }
 }
 
-struct Alertas_Previews: PreviewProvider {
-    static var previews: some View {
+struct Alertas_Previews: PreviewProvider 
+{
+    static var previews: some View 
+    {
         Alertas()
     }
 }
