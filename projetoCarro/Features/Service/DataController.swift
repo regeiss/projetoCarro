4a71ea24b0cc92df7cdea8c0f1100c844156c0dc
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2022-07-23 10:26:27 -0300

    incluido datacontroller

---
+++ projetoCarro/Features/Service/DataController.swift
@@ -0,0 +1,20 @@
+//
+//  DataController.swift
+//  projetoCarro
+//
+//  Created by Roberto Edgar Geiss on 23/07/22.
+//
+
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Abastecimento")
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
