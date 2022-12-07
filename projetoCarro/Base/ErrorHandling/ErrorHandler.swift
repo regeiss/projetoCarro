d7d58ea15c48f5cfc7f45bf1d31fdf8a1b0b3c81
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2022-12-07 16:43:49 -0300

    Update ErrorHandler.swift

--- projetoCarro/Base/ErrorHandling/ErrorHandler.swift
+++ projetoCarro/Base/ErrorHandling/ErrorHandler.swift
@@ -11,13 +11,34 @@ import SwiftUI
 enum ValidationError: LocalizedError
 {
     case missingName
-
+    case wrongDataFormat(error: Error)
+    case missingData
+    case creationError
+    case batchInsertError
+    case batchDeleteError
+    case persistentHistoryChangeError
+    case unexpectedError(error: Error)
+    
     var errorDescription: String?
     {
         switch self
         {
             case .missingName:
                 return "Name is a required field."
+                    case .wrongDataFormat(let error):
+            return NSLocalizedString("Could not digest the fetched data. \(error.localizedDescription)", comment: "")
+        case .missingData:
+            return NSLocalizedString("Found and will discard a quake missing a valid code, magnitude, place, or time.", comment: "")
+        case .creationError:
+            return NSLocalizedString("Failed to create a new Quake object.", comment: "")
+        case .batchInsertError:
+            return NSLocalizedString("Failed to execute a batch insert request.", comment: "")
+        case .batchDeleteError:
+            return NSLocalizedString("Failed to execute a batch delete request.", comment: "")
+        case .persistentHistoryChangeError:
+            return NSLocalizedString("Failed to execute a persistent history change request.", comment: "")
+        case .unexpectedError(let error):
+            return NSLocalizedString("Received unexpected error. \(error.localizedDescription)", comment: "")
         }
     }
 }
